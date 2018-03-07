55b4d9fbad4005fb9e80f5d3bf27c7e53224ffba
desertlion <fikri.desertlion@gmail.com>
2018-03-07 15:40:41 +0700

    Add image styling, add colorbox plugin for image overlay

--- index.js
+++ index.js
@@ -22,6 +22,11 @@ $(function(){
       options: {
         loginSuccessCallback,
         newMessagesCallback,
+        /* 
+          If you're confused with code style above, don't
+          It's just ES6, in ES5 you wrote object like this obj = { a: a, b: b}
+          in ES6 you can wrote obj = { a, b }
+        */
       }
     });
     qiscus.setUser(userId, password, displayName);
@@ -37,6 +42,14 @@ $(function(){
         setTimeout(() => scrollToBottom, 0);
       });
   })
+
+  // add lightbox for images
+  // $('.qcw-container').find('a.lightbox').colorbox();
+  $('#qiscus-widget').on('click', '.lightbox', function() {
+    console.log('clicked')
+    $.colorbox({href:$(this).attr('href'), open:true});
+    return false;
+  });
 });
 
 function loginSuccessCallback() {
@@ -73,6 +86,13 @@ function renderComment(comment, commentBefore) {
   const isTop = commentBefore != null 
     ? comment.username_real !== commentBefore.username_real
     : true;
+  const isFile = comment.message.substring(0,6) == '[file]';
+  let commentMessage = comment.message;
+  if(isFile) {
+    const messageURI =  comment.message.substring(6,comment.message.length-7).trim();
+    const isImage    =  isFile && ['jpg','gif','jpeg','png'].includes(messageURI.split('.').pop().toLowerCase());
+    commentMessage = `<a class="lightbox" href="${messageURI}"><img src="${messageURI}" class="qcw-image-attachment" /></a>`
+  }
   let commentClass = `qcw-comment__message`;
   if(isMe) commentClass += ` qcw-comment__message--me`;
   if(isTop) commentClass += ` qcw-comment__message--top`;
@@ -81,7 +101,7 @@ function renderComment(comment, commentBefore) {
       <img src="${comment.avatar}" class="qcw-avatar" />
       <div class="qcw-comment__bubble">
         <strong>${comment.username_real}</strong>
-        ${comment.message}
+        <div class="qcw-comment__text">${commentMessage}</div>
       </div>
       <div class="qcw-comment__meta">
         <div class="qcw-comment__meta-time">${comment.time}</div>
